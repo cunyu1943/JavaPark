174e0c381a3f2bfa7c5f13132ded244412acfe4d
村雨遥 <747731461@qq.com>
2023-09-01 08:21:43 +0800

    🚑 周刊#20 & 升级

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -95,7 +95,7 @@ export default hopeTheme({
     // 版权信息
     copyright: {
       global: true,
-      triggerWords: 20,
+      triggerLength: 20,
       license: "著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处",
     },
 
