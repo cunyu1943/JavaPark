e416e777a61be50fa954becc91a25f56684e0ca3
cunyu1943 <747731461@qq.com>
2023-10-10 20:11:49 +0800

    ⬆️ 主题 238

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -129,7 +129,7 @@ export default hopeTheme({
       playground: {
         presets: ["ts", "vue"],
       },
-      presentation: ["highlight", "math", "search", "notes", "zoom"],
+      revealjs: ["highlight", "math", "search", "notes", "zoom"],
       stylize: [
         {
           matcher: "Recommended",
