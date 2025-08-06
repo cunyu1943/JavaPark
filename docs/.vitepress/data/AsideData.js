aa5fdf8ae1a8de3d2abb11d2a5994135d23b2035
村雨遥 <747731461@qq.com>
2025-08-06 21:33:34 +0800

    🎈 perf: 侧边栏广告

--- docs/.vitepress/data/AsideData.js
+++ docs/.vitepress/data/AsideData.js
@@ -6,17 +6,40 @@ export const Aside_Data = [
         image: 'https://cunyu1943.github.io/weekly/icon/gratewall.svg',
         info1: '稳定机场，已使用多年',
         promo: '✈️',
+        // info2: '年付 64折 优惠码  2461880'
     },
     {
         link: 'https://ym.ksjhaoka.com/?s=Ltxbbs9W330131',
         image: 'https://cunyu1943.github.io/weekly/icon/sim.svg',
         info1: '办理超低资费超大流量卡',
         promo: '官方正规卡号',
+        // info2: '年付 64折 优惠码  2461880'
     },
     {
         link: 'https://yuque.com/cunyu1943',
         image: 'https://cunyu1943.github.io/weekly/icon/yuque.svg',
         info1: '填写邀请码即送30天会员',
         promo: '邀请码 VUFNKN',
+        // info2: '年付 64折 优惠码  2461880'
+    },
+    {
+        link: 'https://login.123pan.com/centerlogin?register=invite&ref=QOFavd',
+        image: 'https://cunyu1943.github.io/weekly/icon/123.svg',
+        info1: '2T超大云盘容量！免费领',
+        promo: '☁️',
+        // info2: '年付 64折 优惠码  2461880'
     }
+
+    // {
+    //     link: 'https://xx.theojs.cn/',
+    //     icon: 'https://i.theojs.cn/avatar.png',
+    //     name: '玄学宝典',
+    //     hide1: '传世经典著作',
+    //     hide2: '山医命相卜'
+    // },
+    // {
+    //     link: 'https://github.com/sponsors/cunyu1943',
+    //     icon: 'https://doc.theojs.cn/爱心.png',
+    //     name: '为我赞助'
+    // }
 ]
